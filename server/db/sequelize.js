1aef49e83a77f9a1946dee2944282ae8b6516f20
SUNG <paul_sung@keysight.com>
2018-07-11 16:04:58 -0700

    sequelize

--- server/db/sequelize.js
+++ server/db/sequelize.js
@@ -4,10 +4,27 @@ var Sequelize = require('sequelize')
 
 const config = require('./config').config;
 
-const sequelize = new Sequelize(config.dbname, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect
-});
+// const sequelize = new Sequelize(config.dbname, config.username, config.password, {
+//   host: config.host,
+//   dialect: config.dialect
+// });
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    // port:     match[4],
+    // host:     match[3],
+    // logging:  true //false,
+    dialectOptions: {
+      ssl: true
+  }
+  })
+} else {
+  // the application is executed on the local machine ... use mysql
+  sequelize = new Sequelize('postgres://postgres:Sarah412!@localhost:5432/hellodb');
+}
 
 function connect() {
 
