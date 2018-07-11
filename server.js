37101fd6d563bae077c4ae0e05b841a801a75075
SUNG <paul_sung@keysight.com>
2018-07-10 22:16:54 -0700

    qwe

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
-const app = express();
-// const api = require('./server/routes/api');
-
-// set body parsers
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false}));
-
-// serve static files
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static('public'));
-
-// send api requests to the server/routes/api.js file
-// app.use('/api', api);
-
-// send all other requests to the Angular app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
-
-// set the port
-const port = process.env.PORT || '4200';
-app.set('port', port);
-
-app.set('Content-Type', 'text/plain');
-
-
-// start the server
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
-console.log('app')
-console.log(app)
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const path = require('path');
 // const http = require('http');
-// const PORT = process.env.PORT || 5000;
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-// server.listen(PORT, () => {
-//   console.log(`Server running on ${PORT}/`);
-// });
+// const app = express();
+// // const api = require('./server/routes/api');
+
+// // set body parsers
+// // app.use(bodyParser.json());
+// // app.use(bodyParser.urlencoded({ extended: false}));
+
+// // serve static files
+// app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static('public'));
+
+// // send api requests to the server/routes/api.js file
+// // app.use('/api', api);
+
+// // send all other requests to the Angular app
+// // app.get('*', (req, res) => {
+// //   res.sendFile(path.join(__dirname, 'dist/index.html'));
+// // });
+
+// // set the port
+// const port = process.env.PORT || '4200';
+// app.set('port', port);
+
+// app.set('Content-Type', 'text/plain');
+
+
+// // start the server
+// const server = http.createServer(app);
+// server.listen(port, () => console.log(`Running on localhost:${port}`));
+// console.log('app')
+// console.log(app)
+
+
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
