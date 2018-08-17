b0b2aa75743aaad4475094b9ee3eca42296a2816
k6116 <naekyung@gmail.com>
2018-08-17 08:28:01 -0700

    Create testString.py

---
+++ src/assets/python/testString.py
@@ -0,0 +1,20 @@
+import sys, json
+
+#Read data from stdin
+def read_in():
+    lines = sys.stdin.readlines()
+    # Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
+def main():
+    #get our data as an array from read_in()
+    lines = read_in()
+
+    for item in lines:
+        print(item)
+    #return the sum to the output stream
+    
+
+# Start process
+if __name__ == '__main__':
+    main()w
